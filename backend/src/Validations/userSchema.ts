23738aa62b3a9daf470de95715be87f051c69b30
Muhammad Ahmad <mahmadmalik0301@gmail.com>
2025-08-11 12:40:32 +0500

    some error mix

--- backend/src/Validations/userSchema.ts
+++ backend/src/Validations/userSchema.ts
@@ -3,17 +3,17 @@ import Joi from "joi";
 const userSchema = Joi.object({
   first_name: Joi.string().trim().min(2).max(50).required(),
   last_name: Joi.string().trim().min(2).max(50).required(),
-  age: Joi.number().integer().min(1).max(120).required(),
+  age: Joi.number().integer().min(1).max(120).optional(),
   phone_number: Joi.string()
     .pattern(/^[0-9+\-\s()]{7,20}$/)
-    .required()
+    .optional()
     .messages({
       "string.pattern.base":
         "Phone number must be valid and between 7-20 characters.",
     }),
   email: Joi.string().email().required(),
-  country: Joi.string().trim().min(2).max(50).required(),
-  address: Joi.string().trim().min(5).max(255).required(),
+  country: Joi.string().trim().min(2).max(50).optional(),
+  address: Joi.string().trim().min(5).max(255).optional(),
 });
 
 export default userSchema;
